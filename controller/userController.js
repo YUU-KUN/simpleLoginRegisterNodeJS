1cc345917742f0a3822bcf8d40cc2d83b346d01e
YUU-KUN <indra_wahyu_27rpl@student.smktelkom-mlg.sch.id>
2020-09-12 11:12:56 +0700

    update: penambahan fitur session

--- controller/userController.js
+++ controller/userController.js
@@ -5,10 +5,22 @@ const mongoose = require('mongoose');
 const User = mongoose.model('User')
 
 router.get('/', function (req, res) {
-    res.render('userLogin')
+    if (req.session.username && req.session.email) {
+        res.redirect('/home')
+    } else {
+        res.render('userLogin')
+    }
 })
 router.get('/home', function (req, res) {
-    res.render('Home')
+    if (req.session.email) {
+        res.render('Home', {
+            username: req.session.username,
+            email: req.session.email
+        })
+    } else {
+        res.redirect('/');
+        console.log('Mohon login terlebih dahulu!')
+    }
 })
 
 router.get('/register', function (req, res) {
@@ -20,20 +32,28 @@ router.post('/userRegister', function (req, res) {
         tambahUser(req, res)
     } else {
         res.redirect('/')
-        console.log('Error registering user');
+        console.log('Error registering user')
+        // console.log('Error registering user');
     }
 })
 router.post('/userLogin', function (req, res) {
     User.findOne({ email: req.body.email, password: req.body.password }, function (err, user) {
         if (!err) {
             if (user) {
+                // console.log(user)
+                // console.log(user.username)
+                req.session.username = user.username
+                req.session.email = req.body.email
                 res.redirect('/home')
-                console.log('Berhasil Login')
+                console.log('Berhasil Login. Selamat Datang ' + req.body.email)
+                // console.log('Berhasil Login')
             } else {
-                res.render('500').send('User belum terdaftar')
+                console.log('User belum terdaftar')
+                res.status('500').send('User belum terdaftar')
             }
         } else {
-            res.render('500').send('Kesalahan pada Data')
+            console.log('Terdapat kesalahan pada data')
+            res.status('500').send('Kesalahan pada Data')
         }
     })
 })
@@ -47,9 +67,15 @@ function tambahUser(req, res) {
         if (!err) {
             res.redirect('/')
         } else {
-            console.log('Error registering user');
+            console.log('Error registering user')
+            // console.log('Error registering user');
         }
     })
 }
+
+router.get('/logout', function (req, res) {
+    req.session = null
+    res.redirect('/')
+})
 
 module.exports = router
--- server.js
+++ server.js
@@ -2,12 +2,21 @@ require('./models/db');
 
 const express = require('express');
 const path = require('path');
+const cookieSession = require('cookie-session')
+// const session = require('express-session');
+
 // const exphbs = require('express-handlebars')
 const bodyparser = require('body-parser');
 
 const userController = require('./controller/userController');
 
 var app = express();
+// app.use(session({secret: 'ssshhhhh',saveUninitialized: true,resave: true}));
+
+app.use(cookieSession({
+    name: 'session',
+    keys: ['key1', 'key2']
+}))
 app.use(bodyparser.urlencoded({
     extended: true  
 }));
